edbf4e47386cc6c525123b61bf29d50803942141
Ben Brookes <brookesb91@gmail.com>
2019-04-11 15:24:42 +0100

    Fixes

--- server.js
+++ server.js
@@ -1,53 +1,43 @@
 const express = require('express'),
-http = require('http'),
-app = express(),
-server = http.createServer(app),
-io = require('socket.io').listen(server);
+  http = require('http'),
+  app = express(),
+  server = http.createServer(app),
+  io = require('socket.io').listen(server);
+
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-
-res.send('Chat Server is running on port 3000')
-});
-io.on('connection', (socket) => {
-
-console.log('user connected')
-
-socket.on('join', function(userNickname) {
-
-        console.log(userNickname +" : has joined the chat "  );
-
-        socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
-    });
-
-
-socket.on('messagedetection', (senderNickname,messageContent) => {
-       
-       //log the message in console 
-
-       console.log(senderNickname+" :" +messageContent)
-        //create a message object 
-       let  message = {"message":messageContent, "senderNickname":senderNickname}
-          // send the message to the client side  
-       io.emit('message', message );
-     
-      });
-      
-  
- socket.on('disconnect', function() {
-    console.log( ' user has left ')
-    socket.broadcast.emit("userdisconnect"," user has left ") 
-
+  res.send(`Chat Server is running on port ${port}`);
 });
 
+io.on('connection', socket => {
+  console.log('user connected');
 
+  socket.on('join', function(userNickname) {
+    console.log(userNickname + ' : has joined the chat ');
 
+    socket.broadcast.emit(
+      'userjoinedthechat',
+      userNickname + ' : has joined the chat '
+    );
+  });
+
+  socket.on('messagedetection', (senderNickname, messageContent) => {
+    //log the message in console
+
+    console.log(senderNickname + ' :' + messageContent);
+    //create a message object
+    let message = { message: messageContent, senderNickname: senderNickname };
+    // send the message to the client side
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', function() {
+    console.log(' user has left ');
+    socket.broadcast.emit('userdisconnect', ' user has left ');
+  });
 });
 
-
-
-
-
-server.listen(3000,()=>{
-
-console.log('Node app is running on port 3000');
-
+server.listen(port, () => {
+  console.log(`Node app is running on ${port}`);
 });
